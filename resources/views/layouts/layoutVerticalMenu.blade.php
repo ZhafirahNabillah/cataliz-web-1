d5fe466e43cfbcd563eab44136919f56280a1059
DickyFattah <55137955+DickyFattah@users.noreply.github.com>
2021-02-03 10:47:25 +0700

    hapus kotak menu kecil kanan atas

--- resources/views/layouts/layoutVerticalMenu.blade.php
+++ resources/views/layouts/layoutVerticalMenu.blade.php
@@ -1,6 +1,9 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>tinymce.init({selector:'textarea'});</script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
