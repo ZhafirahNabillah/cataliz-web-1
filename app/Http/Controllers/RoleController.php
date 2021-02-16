40d5675a691d6abf7e4f2c2f358969ab843b599d
hafiyannn <55193476+hafiyyann@users.noreply.github.com>
2021-02-16 16:11:40 +0700

    update role, permission, user management

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use DataTables;
+
+class RoleController extends Controller
+{
+    //
+
+    public function index(Request $request)
+    {
+      if ($request->ajax()) {
+        $data = Role::all();
+
+        return DataTables::of($data)
+          ->addIndexColumn()
+          ->addColumn('action', function ($row) {
+            $edit_btn = '<a href="javascript:;" id="editRole" class="btn-sm btn-primary" data-id="' . $row->id . '" data-original-title="detail feedback">Update</a>';
+            $delete_btn = '<a href="javascript:;" id="deleteRole" class="btn-sm btn-primary" data-id="' . $row->id . '" data-original-title="detail feedback">Delete</a>';
+            $actionBtn = $edit_btn.' '.$delete_btn;
+            return $actionBtn;
+          })
+          ->rawColumns(['action'])
+          ->make(true);
+      }
+
+      $permissions = Permission::all();
+      return view('roles.index', compact('permissions'));
+    }
+
+    public function edit($id)
+    {
+      //
+      $role = Role::find($id);
+      return response()->json($role);
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required|unique:roles,name',
+            'permission' => 'required',
+        ]);
+
+        $role = Role::create(['name' => $request->input('name')]);
+
+        $role->syncPermissions($request->input('permission'));
+
+        return response()->json(['success' => 'Customer saved successfully!']);
+    }
+
+    public function destroy($id){
+      $role = Role::find($id);
+      $role->syncPermissions();
+      $role->delete();
+      
+      return response()->json(['success' => 'Client deleted!']);
+    }
+}
