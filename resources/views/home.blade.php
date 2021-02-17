c5db4a7b367d4732c18f783ae681e51c9fd8ec4b
DickyFattah <55137955+DickyFattah@users.noreply.github.com>
2021-02-17 19:16:29 +0700

    update dashboard admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,128 +16,103 @@
     </div>
     <div class="content-body">
 
-      <!--card -->
       @role('admin')
-      @extends('layouts.layoutVerticalMenu')
-
-      @section('title','Home')
-
-      @section('content')
-
-      @include('panels.navbar')
-
-      @include('panels.sidemenu')
-
-      <div class="app-content content ">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
-        <div class="content-wrapper">
-          <div class="content-header row">
-          </div>
-          <div class="content-body">
-
-            <!--card -->
-            <section id="card-demo-example">
-              <div class="row match-height">
-                <div class="container">
-                  <div class="row justify-content-left">
-                    <div class="col-md-3">
-                      <div class="card">
-                        <div class="card-body">
-                          @if (session('status'))
-                          <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                          </div>
-                          @endif
-
-                          {{ __('You are logged in!') }}
-                        </div>
-                      </div>
+      <section id="card-demo-example">
+        <div class="row match-height">
+          <div class="container">
+            <div class="row justify-content-left">
+              <div class="col-md-3">
+                <div class="card">
+                  <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                      {{ session('status') }}
                     </div>
-                  </div>
-                </div>
-                <div class="col-md-4 col-lg-3">
-                  <div class="card">
-                    <div class="card-body">
-                      <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 172.png') }}" alt="Card image cap" />
-                      <small class="card text-center text-muted mb-1">Total Coach
-                      </small>
-                      <img class="rounded float-right width=" 20" height="20"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Jumlah waktu mengajar yang telah dilaksanakan" />
-                      <h2 class="font-weight-bolder text-center">... Coach</h2>
+                    @endif
 
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-4 col-lg-3">
-                  <div class="card">
-                    <div class="card-body">
-                      <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 115.png') }}" alt="Card image cap" />
-                      <small class="card text-center text-muted mb-1">Total Coachee
-                      </small>
-                      <img class="rounded float-right width=" 20" height="20"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Jumlah coachee" />
-                      <h2 class="font-weight-bolder text-center">... Coachee</h2>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-md-4 col-lg-3">
-                  <div class="card">
-                    <div class="card-body">
-                      <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 191.png') }}" alt="Card image cap" />
-                      <small class="card text-center text-muted mb-1">Total Plan
-                      </small>
-                      <img class="rounded float-right width=" 20" height="20"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="" />
-                      <h2 class="font-weight-bolder text-center">... Plan</h2>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-md-4 col-lg-3">
-                  <div class="card">
-                    <div class="card-body">
-                      <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 90.png') }}" alt="Card image cap" />
-                      <small class="card text-center text-muted mb-1">Total Session
-                      </small>
-                      <img class="rounded float-right width=" 20" height="20"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Total sesi yang telah dilaksanakan" />
-                      <h2 class="font-weight-bolder text-center">... Sessions</h2>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-md-12 col-lg-12">
-                  <div class="card">
-                    <div class="card-body">
-                      <h5 class="card-title mb-1">List Agenda</h5>
-                      <table class="datatables-basic table yajra-datatable1">
-                        <thead>
-                          <tr>
-                            <th>No</th>
-                            <th>Name</th>
-                            <th>Session</th>
-                            <th>Date</th>
-                            <th>Duration</th>
-                            <th>Status</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                      </table>
-                    </div>
+                    {{ __('You are logged in!') }}
                   </div>
                 </div>
               </div>
-            </section>
-            <!-- /card -->
+            </div>
+          </div>
+          <div class="col-md-4 col-lg-3">
+            <div class="card">
+              <div class="card-body">
+                <div class="card-title">
+                  <img class="rounded float-right width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Jumlah coach yang terdaftar" />
+                </div>
+                <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 172.png') }}" alt="Card image cap" />
+                <small class="card text-center text-muted mb-1">Total Coach
+                </small>
+                <h2 class="font-weight-bolder text-center">... Coach</h2>
 
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-lg-3">
+            <div class="card">
+              <div class="card-body">
+                <img class="rounded float-right width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Jumlah client coachee yang terdaftar" />
+
+                <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 115.png') }}" alt="Card image cap" />
+                <small class="card text-center text-muted mb-1">Total Coachee
+                </small>
+                <h2 class="font-weight-bolder text-center">... Coachee</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-lg-3">
+            <div class="card">
+              <div class="card-body">
+                <img class="rounded float-right width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Jumlah plan yang terdaftar" />
+
+                <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 191.png') }}" alt="Card image cap" />
+                <small class="card text-center text-muted mb-1">Total Plan
+                </small>
+                <h2 class="font-weight-bolder text-center">... Plan</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-4 col-lg-3">
+            <div class="card">
+              <div class="card-body">
+                <img class="rounded float-right width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Jumlah sesi yang terdaftar" />
+
+                <img class="rounded mx-auto d-block center" src="{{ url('assets\images\icons\admin\Group 90.png') }}" alt="Card image cap" />
+                <small class="card text-center text-muted mb-1">Total Session
+                </small>
+                <h2 class="font-weight-bolder text-center">... Sessions</h2>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-12 col-lg-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title mb-1">List Agenda</h5>
+                <table class="datatables-basic table yajra-datatable1">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Name</th>
+                      <th>Session</th>
+                      <th>Date</th>
+                      <th>Duration</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-
-
-      <div class="sidenav-overlay"></div>
-      <div class="drag-target"></div>
-      @endsection
+      </section>
+      <!-- /card -->
       @endrole
 
       @role('coach')
