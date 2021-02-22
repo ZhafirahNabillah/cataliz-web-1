fc725d578d3e0f6d8127e50c0bb8c25367e8ebd9
DickyFattah <55137955+DickyFattah@users.noreply.github.com>
2021-02-22 20:17:23 +0700

    update button

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -8,51 +8,53 @@ use DataTables;
 
 class PermissionController extends Controller
 {
+  //
+  public function index(Request $request)
+  {
+    if ($request->ajax()) {
+      $data = Permission::all();
+
+      return DataTables::of($data)
+        ->addIndexColumn()
+        ->addColumn('action', function ($row) {
+          $edit_btn = '<a href="javascript:;" id="editPermission" class="btn-sm btn-primary" data-id="' . $row->id . '" data-original-title="detail feedback">Update</a>';
+          $delete_btn = '<a href="javascript:;" id="deletePermission" class="btn-sm btn-danger" data-id="' . $row->id . '" data-original-title="detail feedback">Delete</a>';
+          $actionBtn = $edit_btn . ' ' . $delete_btn;
+          return $actionBtn;
+        })
+        ->rawColumns(['action'])
+        ->make(true);
+    }
+
+    return view('permissions.index');
+  }
+
+  public function edit($id)
+  {
     //
-    public function index(Request $request)
-    {
-      if ($request->ajax()) {
-        $data = Permission::all();
+    $permission = Permission::find($id);
+    return response()->json($permission);
+  }
 
-        return DataTables::of($data)
-          ->addIndexColumn()
-          ->addColumn('action', function ($row) {
-            $edit_btn = '<a href="javascript:;" id="editPermission" class="btn-sm btn-primary" data-id="' . $row->id . '" data-original-title="detail feedback">Update</a>';
-            $delete_btn = '<a href="javascript:;" id="deletePermission" class="btn-sm btn-primary" data-id="' . $row->id . '" data-original-title="detail feedback">Delete</a>';
-            $actionBtn = $edit_btn.' '.$delete_btn;
-            return $actionBtn;
-          })
-          ->rawColumns(['action'])
-          ->make(true);
-      }
+  public function store(Request $request)
+  {
+    $this->validate($request, [
+      'name'  => 'required',
+    ]);
 
-      return view('permissions.index');
-    }
+    Permission::updateOrCreate(
+      ['id' => $request->input('permission_id')],
+      ['name' => $request->input('name')]
+    );
 
-    public function edit($id)
-    {
-      //
-      $permission = Permission::find($id);
-      return response()->json($permission);
-    }
+    return response()->json(['success' => 'Permission saved successfully!']);
+  }
 
-    public function store(Request $request){
-        $this->validate($request, [
-            'name'  => 'required',
-        ]);
+  public function destroy($id)
+  {
+    $permission = Permission::find($id);
+    $permission->delete();
 
-        Permission::updateOrCreate(
-          ['id' => $request->input('permission_id')],
-          ['name' => $request->input('name')]
-        );
-
-        return response()->json(['success' => 'Permission saved successfully!']);
-    }
-
-    public function destroy($id){
-      $permission = Permission::find($id);
-      $permission->delete();
-
-      return response()->json(['success' => 'Permission deleted!']);
-    }
+    return response()->json(['success' => 'Permission deleted!']);
+  }
 }
