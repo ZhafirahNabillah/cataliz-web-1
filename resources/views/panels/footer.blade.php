cf985037a5d67bc5cbda9ec6faa8a7bbc85ebdc3
hafiyannn <naufalfiyyan@gmail.com>
2021-03-17 13:18:56 +0700

    update footer

--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -1,10 +1,6 @@
 <!-- BEGIN: Footer-->
     <footer class="footer footer-static footer-light">
-<<<<<<< HEAD
         <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; {{ Carbon\Carbon::now()->year }}<a class="ml-25" href="https://cataliz.id" target="_blank">Cataliz.id </a><span class="d-none d-sm-inline-block">| All rights Reserved</span></span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
-=======
-        <p class="clearfix mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; {{date("Y")}}<a class="ml-25" href="https://1.envato.market/pixinvent_portfolio" target="_blank">Pixinvent</a><span class="d-none d-sm-inline-block">, All rights Reserved</span></span><span class="float-md-right d-none d-md-block">Hand-crafted & Made with<i data-feather="heart"></i></span></p>
->>>>>>> 62aa57800d902a4325e45bec335583b21ca589e7
     </footer>
     <button class="btn btn-primary btn-icon scroll-top" type="button"><i data-feather="arrow-up"></i></button>
     <!-- END: Footer-->
