9c1fd89547e33e1cb923bc3bc1cb5e02f5525a9e
Dicky Fattah <DickyFattah>
2021-03-18 15:33:13 +0700

    email verifikation

--- resources/views/pdf_template/plans_detail_pdf.blade.php
+++ resources/views/pdf_template/plans_detail_pdf.blade.php
@@ -2,251 +2,117 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title>Example 2</title>
-  <style type="text/css">
-    @font-face {
-      font-family: SourceSansPro;
-      src: url(SourceSansPro-Regular.ttf);
-    }
-
-    .clearfix:after {
-      content: "";
-      display: table;
-      clear: both;
-    }
-
-    a {
-      color: #0087C3;
-      text-decoration: none;
-    }
-
-    body {
-      position: relative;
-      width: 21cm;
-      height: 29.7cm;
-      margin: 0 auto;
-      color: #555555;
-      background: #FFFFFF;
-      font-family: Arial, sans-serif;
-      font-size: 14px;
-      font-family: SourceSansPro;
-    }
-
-    header {
-      padding: 10px 0;
-      margin-bottom: 20px;
-      border-bottom: 1px solid #AAAAAA;
-    }
-
-    #logo {
-      float: left;
-      margin-top: 8px;
-    }
-
-    #logo img {
-      height: 70px;
-    }
-
-    #company {
-      float: right;
-      text-align: right;
-    }
-
-
-    #details {
-      margin-bottom: 10px;
-    }
-
-    #client {
-      padding-left: 6px;
-
-      text-align: center;
-    }
-
-    #client .to {
-      color: #777777;
-    }
-
-    h2.name {
-      font-size: 1.4em;
-      font-weight: normal;
-      margin: 0;
-    }
-
-    #invoice {
-      float: right;
-      text-align: right;
-    }
-
-    #invoice h1 {
-      color: #0087C3;
-      font-size: 2.4em;
-      line-height: 1em;
-      font-weight: normal;
-      margin: 0 0 10px 0;
-    }
-
-    #invoice .date {
-      font-size: 1.1em;
-      color: #777777;
-    }
-
-    table {
-      width: 100%;
-      border-collapse: collapse;
-      border-spacing: 0;
-      margin-bottom: 20px;
-    }
-
-    table th,
-    table td {
-      padding: 20px;
-      background: white;
-      text-align: center;
-      border-bottom: 1px solid #FFFFFF;
-    }
-
-    table th {
-      white-space: nowrap;
-      font-weight: normal;
-    }
-
-    table td {
-      text-align: right;
-    }
-
-    table td h3 {
-      color: #57B223;
-      font-size: 1.2em;
-      font-weight: normal;
-      margin: 0 0 0.2em 0;
-    }
-
-    table .no {
-      color: #FFFFFF;
-      font-size: 1.6em;
-      background: #57B223;
-    }
-
-    table .desc {
-      text-align: left;
-    }
-
-    table .unit {
-      background: #DDDDDD;
-    }
-
-    table .qty {}
-
-    table .total {
-      background: #57B223;
-      color: #FFFFFF;
-    }
-
-    table td.unit,
-    table td.qty,
-    table td.total {
-      font-size: 1.2em;
-    }
-
-    table tbody tr:last-child td {
-      border: none;
-    }
-
-    table tfoot td {
-      padding: 10px 20px;
-      background: #FFFFFF;
-      border-bottom: none;
-      font-size: 1.2em;
-      white-space: nowrap;
-      border-top: 1px solid #AAAAAA;
-    }
-
-    table tfoot tr:first-child td {
-      border-top: none;
-    }
-
-    table tfoot tr:last-child td {
-      color: #57B223;
-      font-size: 1.4em;
-      border-top: 1px solid #57B223;
-
-    }
-
-    table tfoot tr td:first-child {
-      border: none;
-    }
-
-    #thanks {
-      font-size: 2em;
-      margin-bottom: 50px;
-    }
-
-    #notices {
-      padding-left: 6px;
-      border-left: 6px solid #0087C3;
-    }
-
-    #notices .notice {
-      font-size: 1.2em;
-    }
-
-    footer {
-      color: #777777;
-      width: 100%;
-      height: 30px;
-      position: absolute;
-      bottom: 0;
-      border-top: 1px solid #AAAAAA;
-      padding: 8px 0;
-      text-align: center;
-    }
-  </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Laravel 7 PDF Example</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  {{--
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" /> --}}
 </head>
 
 <body>
-  <header class="clearfix" style="background-color: #F9ECC0;">
-    <div id="logo" style="padding-left:10px ;">
-      <img src="">
+  <main role="main" class="container tex">
+    <!-- head -->
+    <div class="row">
+      <table align="center" style="width: 80%;border: 1px solid white;">
+        <tr>
+          <td>
+            <div class="container" style="margin-top: 20px;margin-left: 10px;">
+              <div style="margin-bottom: 15px;" class="float-right text-right">
+                <h2 class="name">PT WAHANA INTEGRA <br> NUSANTARA</h2>
+                <span> +62 822-3585-0005</span> <img src="icon/telpon.png" alt=""><br>
+                <span> halo@cataliz.id</span> <img src="icon/mail.png" alt="">
+              </div>
+
+              <h1 class="text-left"><img style="width:170px;margin-top: 10px;margin-left: 15px; " src="icon/logofull.png">
+              </h1>
+          </td>
+        </tr>
+      </table>
     </div>
-    <div id="company">
-      <div style="padding-right: 10px;">
-        <h2 class="name">PT WAHANA INTEGRA <br> NUSANTARA</h2>
-        <span> +62 822-3585-0005</span> <img src="icon/telpon.png" alt=""><br>
-        <span> halo@cataliz.id</span> <img src="icon/mail.png" alt="">
-      </div>
+    <hr color="#C58407" />
+
+    <div class="col-12">
+      <h5 style="padding-top:50px;text-align: center;font-size:40px;color: #C58407;padding-bottom: 50px;">Title</h5>
     </div>
-  </header>
-  <main>
-    <div id="details" class="clearfix">
-      <div id="client">
-        <div class="to">
-          <h1 style="color: #C58407;">Tittle</h1>
+
+    <!-- objective -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card bg-faded">
+          <div class="card-header">
+            <b>Objective</b>
+          </div>
+          <div class="card-body">
+            <p>{!!$plan->objective!!}</p>
+          </div>
         </div>
       </div>
     </div>
-    <table border="0" cellspacing="0" cellpadding="0">
-      <td style="text-align: left;">
-        <p style="color: #C58407;font-size:20px ;">Objektif</p>
-        <p style="text-align: justify;">{!!$plan->objective!!}
-        </p>
-        <p style="color: #C58407;font-size:20px ;">Sukses Indikator</p>
-        <p style="text-align: justify;">{!!$plan->success_indicator!!}
-        </p>
-        <p style="color: #C58407;font-size:20px ;">Pengembangan Area</p>
-        <p style="text-align: justify;">{!!$plan->development_areas!!}
-        </p>
-        <p style="color: #C58407;font-size:20px ;">Support</p>
-        <p style="text-align: justify;">{!!$plan->support!!}
-        </p>
-      </td>
-    </table>
-
+    <br>
+    <!-- success indicator -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card bg-faded">
+          <div class="card-header">
+            <b>Success Indicator</b>
+          </div>
+          <div class="card-body">
+            <p>{!!$plan->success_indicator!!}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <!-- Development areas -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card bg-faded">
+          <div class="card-header">
+            <b>Development Areas</b>
+          </div>
+          <div class="card-body">
+            <p>{!!$plan->development_areas!!}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <!-- Support -->
+    <div class="row">
+      <div class="col-12">
+        <div class="card bg-faded">
+          <div class="card-header">
+            <b>Support</b>
+          </div>
+          <div class="card-body">
+            <p>{!!$plan->support!!}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br>
+    <footer>
+      <table align="center" style="width: 100%;background-color: #656565;">
+        <tr style="text-align: center;height: 100px;">
+          <td style="">
+            <a href="https://www.instagram.com/cataliz.id/">
+              <img style="padding-right: 10px;" src="{{ $message->embed(public_path().'/assets/images/icons/email/blackinstagram.png') }}">
+            </a>
+            <a href="https://cataliz.id/">
+              <img style="padding-left: 10px;" src="{{ $message->embed(public_path().'/assets/images/icons/email/blacklink.png') }}">
+            </a>
+          </td>
+        </tr>
+      </table>
+    </footer>
   </main>
-  <footer style="background-color: #F9ECC0;">
-    Thanks for using <a href="app.cataliz.id">cataliz.id</a>
-  </footer>
+  {{--
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+  <script src="{{ asset('js/app.js') }}" type="text/js"></script> --}}
 </body>
 
 </html>
