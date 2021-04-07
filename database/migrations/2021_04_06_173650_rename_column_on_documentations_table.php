3aede4a14362fbfd4d50169fce322635efaff04f
hafiyannn <naufalfiyyan@gmail.com>
2021-04-07 15:51:56 +0700

    update migration

--- database/migrations/2021_04_06_173650_rename_column_on_documentations_table.php
+++ database/migrations/2021_04_06_173650_rename_column_on_documentations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeColumnAttributeOnDocumentationsTable extends Migration
+class RenameColumnOnDocumentationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -18,8 +18,8 @@ class ChangeColumnAttributeOnDocumentationsTable extends Migration
             //
             $table->renameColumn('menu_id', 'title');
             $table->renameColumn('version', 'description');
-            $table->text('title')->change();
-            $table->longText('description')->change();
+            // $table->text('title')->change();
+            // $table->longText('description')->change();
         });
     }
 
