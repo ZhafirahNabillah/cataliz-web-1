b43b4c828f5cbb6bbc5704add276a1b3c22c06b8
Dicky Fattah <DickyFattah>
2021-04-13 11:40:34 +0700

    update create exercise

--- resources/views/exercise/create.blade.php
+++ resources/views/exercise/create.blade.php
@@ -17,9 +17,7 @@
         <div class="row breadcrumbs-top">
           <div class="col-12">
             <h2 class="content-header-title float-left mb-0">Exercise
-              <img class="align-text width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}"
-                alt="Card image cap" data-toggle="popover" data-placement="top"
-                data-content="Halaman ini menampilkan Exercise yang anda miliki untuk klien ini." />
+              <img class="align-text width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Halaman ini menampilkan Exercise yang anda miliki untuk klien ini." />
             </h2>
             <div class="breadcrumb-wrapper">
               <ol class="breadcrumb">
@@ -35,33 +33,36 @@
         </div>
       </div>
     </div>
-    <div class="card">
-      <div class="card-body">
-        <h4 class="card-title">Input jumlah Soal</h4>
-        <p class="card-text"></p>
-        <form>
-          <div class="form-row">
-            <div class="form-group col-md-8">
-              <input type="number" name="" class="input form-control" id="form_numbers"
-                placeholder="Enter number of forms" required>
-            </div>
-            <div class="form-group col-md-4">
-              <div class="text-center">
-                <button class="btn btn-sm btn-primary generate" type="submit">Generate</button>
+    <div class="row ">
+      <div class="col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">Input jumlah Soal</h4>
+            <p class="card-text"></p>
+            <form>
+              <div class="form-row">
+                <div class="form-group col-md-8">
+                  <input type="number" name="" class="input form-control" id="form_numbers" placeholder="Enter number of forms" required>
+                </div>
+                <div class="form-group col-md-4">
+                  <div class="text-center">
+                    <button class="btn btn-sm btn-primary generate" type="submit">Generate</button>
+                  </div>
+                </div>
+            </form>
+          </div>
+        </div>
+        <div class="card ">
+          <div class="card-body">
+            <form>
+              <div class="forms">
               </div>
-            </div>
-        </form>
-      </div>
-    </div>
-    <div class="card ">
-      <div class="card-body">
-        <form>
-          <div class="forms">
+              <div class="text-left">
+                <button class="btn btn-sm btn-primary" type="submit">Submit</button>
+              </div>
+            </form>
           </div>
-          <div class="text-left">
-            <button class="btn btn-sm btn-primary" type="submit">Submit</button>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
 
@@ -79,15 +80,13 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.min.css" id="theme-styles">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.5.0/dist/sweetalert2.all.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.js"></script>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
   </script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
   </script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
   </script>
+  <script src="https://cdn.tiny.cloud/1/8kkevq83lhact90cufh8ibbyf1h4ictwst078y31at7z4903/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 
 
   <script type="text/javascript">
@@ -106,13 +105,61 @@
         // console.log(input);
         let temp_html = '';
         for (i = 0; i < input; i++) {
-          temp_html += '<div class="card "><div class="card-body"><div class="form-group"><label for="">Topic</label><input type="text" class="form-control col-sm-6" id="" aria-describedby="emailHelp" placeholder="Choose your topic"></div><div class="form-group"><label for="">Soal</label><input type="text" class="form-control col-sm-12" id="" placeholder="Input your question..."></div><div class="form-group"><label for="">Answer A</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer B</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer C</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer D</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer E</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div></div></div>';
+          temp_html += '<div class="card "><div class="card-body"><div class="form-group"><label for="">Topic</label><input type="text" class="form-control col-sm-6" id="" aria-describedby="emailHelp" placeholder="Choose your topic"></div><div class="form-group"><label for="question">Question</label><textarea name="question" id="question" placeholder="Your question here..."></textarea></div><div class="form-group"><label for="">Answer A</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer B</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer C</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer D</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div><div class="form-group"><label for="">Answer E</label><input type="text" class="form-control col-sm-6" id="" placeholder="Input your Answer..."></div></div></div>';
         }
         $('.forms').append(temp_html);
+
+
+
+        tinymce.init({
+          selector: 'textarea',
+
+          image_class_list: [{
+            title: 'img-fluid',
+            value: 'img-fluid'
+          }, ],
+          height: 700,
+          setup: function(editor) {
+            editor.on('init change', function() {
+              editor.save();
+            });
+          },
+          plugins: [
+            "advlist autolink lists link image charmap print preview anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste imagetools"
+          ],
+          toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image ",
+
+          image_title: true,
+          automatic_uploads: true,
+          images_upload_url: '/docs/upload_image',
+          file_picker_types: 'image',
+          file_picker_callback: function(cb, value, meta) {
+            var input = document.createElement('input');
+            input.setAttribute('type', 'file');
+            input.setAttribute('accept', 'image/*');
+            input.onchange = function() {
+              var file = this.files[0];
+
+              var reader = new FileReader();
+              reader.readAsDataURL(file);
+              reader.onload = function() {
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache = tinymce.activeEditor.editorUpload.blobCache;
+                var base64 = reader.result.split(',')[1];
+                var blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+                cb(blobInfo.blobUri(), {
+                  title: file.name
+                });
+              };
+            };
+            input.click();
+          }
+        });
       });
     });
-
-
     // popover
     $(function() {
       $('[data-toggle="popover"]').popover({
