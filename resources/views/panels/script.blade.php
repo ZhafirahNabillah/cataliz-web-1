6d69b847a24adade45cdad73e3175b600f3d02bb
Riogunawan62587 <55238065+Riogunawan62587@users.noreply.github.com>
2021-04-13 11:02:45 +0700

    Update exercise form bugs

--- resources/views/panels/script.blade.php
+++ resources/views/panels/script.blade.php
@@ -28,8 +28,9 @@
 </script>
 
 @elseif (in_array(Route::currentRouteName(), ['clients.index', 'plans.index', 'agendas.index', 'clients.show',
-'plans.show', 'dashboard','roles.index','agendas.show_group','permissions.index','users.index', 'class.index', 'class.show',
-'profil','auth.register', 'group.show', 'docs.index', 'topic.index']))
+'plans.show', 'dashboard','roles.index','agendas.show_group','permissions.index','users.index', 'class.index',
+'class.show',
+'profil','auth.register', 'group.show', 'docs.index', 'topic.index','exercise.index','exercise.create']))
 
 
 <!-- BEGIN: Vendor JS-->
