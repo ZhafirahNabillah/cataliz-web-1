6c735d1fb9a327d10d2be01ffd3bb09d91a51443
hafiyannn <naufalfiyyan@gmail.com>
2021-04-22 20:13:18 +0700

    create new migration for answers

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -160,7 +160,7 @@ final class Versions
   'sebastian/type' => '2.3.1@81cd61ab7bbf2de744aba0ea61fae32f721df3d2',
   'sebastian/version' => '3.0.2@c6c1022351a901512170118436c764e473f6de8c',
   'theseer/tokenizer' => '1.2.0@75a63c33a8577608444246075ea0af0d052e452a',
-  'laravel/laravel' => 'dev-main@a78894c4f09edf0f4d2baca21208a752a2c61724',
+  'laravel/laravel' => 'dev-main@8a11c1fdf7eb9cd78e7cb6ac27be883bffeec0dd',
 );
 
     private function __construct()
