840770cb80457301bfd8272f96f4da90a1e9a7b2
Olsaviraagsa <55074173+Olsaviraagsa@users.noreply.github.com>
2021-04-28 04:13:32 +0700

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -35,8 +35,9 @@ task('deploy:secrets', function () {
 });
 
 // Hosts
-host('app.cataliz.id') // Name of the server or domain
+//host('app.cataliz.id') // Name of the server or domain
     //->hostname('52.205.83.22') // Hostname or IP address
+host('18.136.248.105') 
     ->hostname('18.136.248.105') //IP Server akun Cataliz
     ->stage('staging') // Deployment stage (production, staging, etc)
     ->user('ubuntu') // SSH user
