c6037f1c465ddb601faada1dc8e2915324a5fa62
Riogunawan62587 <55238065+Riogunawan62587@users.noreply.github.com>
2021-06-19 11:35:41 +0700

    Update migration url length for media_url in agendas

--- database/migrations/2021_01_04_202920_create_agenda_details_table.php
+++ database/migrations/2021_01_04_202920_create_agenda_details_table.php
@@ -6,37 +6,37 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateAgendaDetailsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('agenda_details', function (Blueprint $table) {
-            $table->id();
-      			$table->integer('agenda_id');
-            $table->string('session_name');
-      			$table->string('topic')->nullable();
-      			$table->date('date')->nullable();
-      			$table->time('time')->nullable();
-      			$table->string('media')->nullable();
-      			$table->string('media_url')->nullable();
-      			$table->integer('duration')->nullable();
-      			$table->string('status');
-            $table->string('attachment',255)->nullable();
-            $table->string('feedback',10000)->nullable();
-      			$table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('agenda_details', function (Blueprint $table) {
+      $table->id();
+      $table->integer('agenda_id');
+      $table->string('session_name');
+      $table->string('topic')->nullable();
+      $table->date('date')->nullable();
+      $table->time('time')->nullable();
+      $table->string('media')->nullable();
+      $table->string('media_url', 500)->nullable();
+      $table->integer('duration')->nullable();
+      $table->string('status');
+      $table->string('attachment', 255)->nullable();
+      $table->string('feedback', 10000)->nullable();
+      $table->timestamps();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('agenda_details');
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('agenda_details');
+  }
 }
