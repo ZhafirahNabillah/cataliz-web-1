3019673eda582ad5a386f8644dd195c004ab2ecf
Aditya Novan Firmansyah <55123299+AdityaNovanF@users.noreply.github.com>
2021-09-06 15:44:51 +0700

    Update create.blade.php

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -28,7 +28,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="card p-2">
-                        <form action="{{ route('docs.store') }}" method="post">
+                        <form action="" method="post">
                             @csrf
                             <div class="form-group">
                                 <label for="name">Name</label>
@@ -41,7 +41,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input class="form-control @error('email') is-invalid @enderror" type="text" email="email" value="{{ old('email') }}" placeholder="Input your email...">
+                                <input class="form-control @error('email') is-invalid @enderror" type="text" name="email" value="{{ old('email') }}" placeholder="Input your email...">
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -50,7 +50,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="whatsapp_number">Whatsapp Number</label>
-                                <input class="form-control @error('whatsapp_number') is-invalid @enderror" type="text" whatsapp_number="whatsapp_number" value="{{ old('whatsapp_number') }}" placeholder="Input your Whatsapp Number...">
+                                <input class="form-control @error('whatsapp_number') is-invalid @enderror" type="text" name="whatsapp_number" value="{{ old('whatsapp_number') }}" placeholder="Input your Whatsapp Number...">
                                 @error('whatsapp_number')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -59,7 +59,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="instance">Instance</label>
-                                <input class="form-control @error('instance') is-invalid @enderror" type="text" instance="instance" value="{{ old('instance') }}" placeholder="Input your instance...">
+                                <input class="form-control @error('instance') is-invalid @enderror" type="text" name="instance" value="{{ old('instance') }}" placeholder="Input your instance...">
                                 @error('instance')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -68,7 +68,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="profession">Profession</label>
-                                <input class="form-control @error('profession') is-invalid @enderror" type="text" profession="profession" value="{{ old('profession') }}" placeholder="Input your profession...">
+                                <input class="form-control @error('profession') is-invalid @enderror" type="text" name="profession" value="{{ old('profession') }}" placeholder="Input your profession...">
                                 @error('profession')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -77,7 +77,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="address">Address</label>
-                                <input class="form-control @error('address') is-invalid @enderror" type="text" address="address" value="{{ old('address') }}" placeholder="Input your address...">
+                                <input class="form-control @error('address') is-invalid @enderror" type="text" name="address" value="{{ old('address') }}" placeholder="Input your address...">
                                 @error('address')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -86,8 +86,52 @@
                             </div>
                             <div class="form-group">
                                 <label for="goals">goals</label>
-                                <input class="form-control @error('goals') is-invalid @enderror" type="text" goals="goals" value="{{ old('goals') }}" placeholder="Input your goals...">
+                                <input class="form-control @error('goals') is-invalid @enderror" type="text" name="goals" value="{{ old('goals') }}" placeholder="Input your goals...">
                                 @error('goals')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="program">Program</label><br>
+                                <input type="radio" name="program" value="starco"> STARCO <br>
+                                <input type="radio" name="program" value="scmp"> SCMP
+                                @error('progam')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="book_demo">Book Demo</label><br>
+                                <input type="checkbox" name="book_demo" value="coaching"> COACHING <br>
+                                <input type="checkbox" name="book_demo" value="training"> TRAINING <br>
+                                <input type="checkbox" name="book_demo" value="mentoring"> MENTORING <br>
+                                @error('book_demo')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="book_date">Date</label>
+                                <input id="book_date" class="form-control @error('book_date') is-invalid @enderror" type="text" name="book_date" value="{{ old('book_date') }}">
+                                @error('date')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="session">Choose a session:</label>
+                                <select id="session" name="session">
+                                    <option value="1">1 Session</option>
+                                    <option value="2">2 Session</option>
+                                    <option value="3">3 Session</option>
+                                    <option value="4">4 Session</option>
+                                </select>
+                                @error('session')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
@@ -122,58 +166,14 @@
 @push('scripts')
 <script src="//cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 <script src="//cdn.tiny.cloud/1/8kkevq83lhact90cufh8ibbyf1h4ictwst078y31at7z4903/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-<script type="text/javascript">
-    $('.category-select').select2({
-        placeholder: 'Select plans',
-        tags: true
-    });
-
-    tinymce.init({
-        selector: 'textarea',
-
-        image_class_list: [{
-            title: 'img-fluid',
-            value: 'img-fluid'
-        }, ],
-        height: 700,
-        setup: function(editor) {
-            editor.on('init change', function() {
-                editor.save();
-            });
-        },
-        plugins: [
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table contextmenu paste imagetools"
-        ],
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image ",
-
-        image_title: true,
-        automatic_uploads: true,
-        images_upload_url: '/docs/upload_image',
-        file_picker_types: 'image',
-        file_picker_callback: function(cb, value, meta) {
-            var input = document.createElement('input');
-            input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
-            input.onchange = function() {
-                var file = this.files[0];
-
-                var reader = new FileReader();
-                reader.readAsDataURL(file);
-                reader.onload = function() {
-                    var id = 'blobid' + (new Date()).getTime();
-                    var blobCache = tinymce.activeEditor.editorUpload.blobCache;
-                    var base64 = reader.result.split(',')[1];
-                    var blobInfo = blobCache.create(id, file, base64);
-                    blobCache.add(blobInfo);
-                    cb(blobInfo.blobUri(), {
-                        title: file.name
-                    });
-                };
-            };
-            input.click();
-        }
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script>
+    $(function() {
+        $('#book_date').datepicker({
+            autoclose: true,
+            daysOfWeekDisabled: [1, 2, 3, 4]
+        });
     });
 </script>
 @endpush
