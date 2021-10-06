c152d9fe18fdb579d8ec0f0616dd5527bb958f49
Rahmat Indrawan <55143841+Rahmatindrawan@users.noreply.github.com>
2021-10-07 01:32:21 +0700

    Update view invoice again

--- resources/views/booking/edit.blade.php
+++ resources/views/booking/edit.blade.php
@@ -78,7 +78,7 @@
                 <div class="row breadcrumbs-top">
                     <div class="col-12">
                         <h2 class="content-header-title float-left mb-0">Edit Book Demo
-                            <img class="align-text width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Pada halaman ini ditampilkan detail log activity dari semua pengguna yang mengakses website ini." />
+                            <img class="align-text width=" 15px" height="15px"" src=" {{asset('assets\images\icons\popovers.png')}}" alt="Card image cap" data-toggle="popover" data-placement="top" data-content="Halaman ini menampilkan daftar booking yang terdaftar dalam website" />
                         </h2>
                         <div class="breadcrumb-wrapper">
                             <ol class="breadcrumb">
--- resources/views/booking/invoice.blade.php
+++ resources/views/booking/invoice.blade.php
@@ -5,12 +5,6 @@
     <title>Invoice Booking</title>
     <link rel="stylesheet" href="style.css" media="all" />
     <style>
-    .clearfix:after {
-        content: "";
-        display: table;
-        clear: both;
-    }
-
     body {
         position: relative;
         width: 21cm;  
@@ -23,25 +17,43 @@
         font-weight:500;
     }
 
-    header {
-        padding: 10px 0;
-        margin-bottom: 30px;
+    .header {
+      background-color: #7367F0;
+      color: #fff;
+      height: 90px;
     }
 
-    #logo {
-        text-align: center;
-        margin-bottom: 10px;
-    }
+  .header-logo {
+    text-align: center;
+    font-size: 36px;
+    padding: 20px 40px;
+  }
 
-    #logo img {
-        width: 795px;
-        height: 91px;
-    }
+  .footer {
+    background-color: #7367F0;
+    width: 100%;
+    height: 90px;
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    color: #fff;
+  }   
 
-    #footer img {
-        width: 795px;
-        height: 71px;
-    }
+  .footer-list {
+    text-align: center;
+    font-size: 36px;
+    padding: 30px 40px;
+  }
+
+  li {
+    list-style: none;
+    font-size:10px;
+  }
+
+  .footer-list li {
+    float: left;
+    text-align: center;
+  }
 
     h1 {
         color: #5D6975;
@@ -50,30 +62,23 @@
         font-weight: normal;
         text-align: center;
         margin: 20px 0 20px 0;
-    }
-
-    footer {
-        color: #5D6975;
-        width: 100%;
-        height: 30px;
-        position: absolute;
-        bottom: 0;
-        border-top: 1px solid #C1CED9;
-        padding: 8px 0;
-        text-align: center;
-    }                           
+    }                       
     </style>
   </head>
   <body>
-    <header class="clearfix">
-      <div id="logo">
-        <img src="{{ url('/assets/images/header_invoice.png') }}">
+      <div class="header">
+        <div class="header-logo">
+        <img src="{{ url('/assets/images/logo.png') }}" style="width:110px;">
+        </div>
       </div>
+      <!-- <div id="logo">
+        <img src="{{ url('/assets/images/header_invoice.png') }}">
+      </div> -->
       <h1>BOOKING DEMO</h1>
       <div id="detail">
         <table border="0">
           <tbody>
-            <tr><td>Code Booking </td><td>:</td><td>{{$code}}</td></tr>
+          <tr><td>Code Booking </td><td>:</td><td>{{$code}}</td></tr>
             <tr><td>Name</td><td>:</td><td>{{$name}}</td></tr>
             <tr><td>Email</td><td>:</td><td>{{$email}}</td></tr>
             <tr><td>Phone</td><td>:</td><td>{{$whatsapp_number}}</td></tr>
@@ -83,63 +88,59 @@
           </tbody>
       </table>
       </div>
-    </header>
-    <main>
-    <table border="3" bordercolor="#7367F0" style="width:100%; background-color:#FFFFFF; border-collapse: collapse;">
-      <tr style="background-color:#D4D0FF;color:#625F6E;">
-      <th style="padding: 20px">Program</th><th>Category & Session</th><th>Date</th>
-      </tr>
-      <tr>
-
-
-        <td style="padding: 30px; text-align: center;">  
-        @foreach ($book_demo as $dataDemo=>$program)
-        {{$program}}
-        @endforeach</td>
-      
-        @if($session_coaching != 0)
-        <td style="padding: 30px; text-align: center;">
-        {{$session_coaching}}</td>
-        @elseif($session_training != 0)
-        <td style="padding: 30px; text-align: center;">
-        {{$session_training}}</td>
-        @elseif($session_mentoring != 0)
-        <td style="padding: 30px; text-align: center;">
-        {{$session_mentoring}}</td>
-        @endif
-  
-        <td style="padding: 30px; text-align: center;">{{$book_date}}</td>
-      </tr>
-      <tr>
-        <td colspan="2" style="border-left: 0px;"></td>
-        <td style="padding: 10px; background-color:#7367F0; color:#FFFFFF">Total : {{$price}}</td>
-      </tr>
-    </table>
-
-      <!-- <table>
+    <main><br>
+      <table border="3" bordercolor="#7367F0" style="width:100%; background-color:#FFFFFF; border-collapse: collapse;">
         <thead>
-          <tr>
-            <th>Program</th>
+          <tr style="background-color:#D4D0FF;color:#625F6E;">
+            <th style="padding: 20px">Program</th>
             <th>Category & Session</th>
             <th>Date</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td class="service">STARCO</td>
-            <td class="desc">Coaching 1 Session</td>
-            <td class="unit">dd/mm/yy</td>
+        <tr>
+            <td style="padding: 30px; text-align: center;">  
+            @foreach ($book_demo as $dataDemo=>$program)
+            {{$program}}
+            @endforeach</td>
+
+            @if($session_coaching != 0)
+            <td style="padding: 30px; text-align: center;">
+            {{$session_coaching}}</td>
+            @elseif($session_training != 0)
+            <td style="padding: 30px; text-align: center;">
+            {{$session_training}}</td>
+            @elseif($session_mentoring != 0)
+            <td style="padding: 30px; text-align: center;">
+            {{$session_mentoring}}</td>
+            @endif
+            <td style="padding: 30px; text-align: center;">{{$book_date}}</td>
           </tr>
           <tr>
-            <td colspan="2" class="grand total">Total</td>
-            <td class="grand total">Rp.400.000</td>
+            <td colspan="2" style="border-left: 0px;"></td>
+            <td style="padding: 10px; background-color:#7367F0; color:#FFFFFF">Total : {{$price}}</td>
           </tr>
+          <!-- <tr>
+            <td class="service" style="padding: 30px; text-align: center;">STARCO</td>
+            <td class="desc" style="padding: 30px; text-align: center;">Coaching 1 Session</td>
+            <td class="unit" style="padding: 30px; text-align: center;">dd/mm/yy</td>
+          </tr>
+          <tr>
+            <td colspan="2" class="grand total" style="padding: 30px; text-align: center;">Total</td>
+            <td class="grand total" style="padding: 30px; text-align: center;">Rp.400.000</td>
+          </tr> -->
         </tbody>
-      </table> -->
+      </table>
     </main>
     <footer>
-    <div id="footer">
-        <img src="{{ url('/assets/images/footer_invoice.png') }}">
+    <div class="footer">
+      <div class="footer-list">
+        <li style="padding: 10px 25px;">PT WAHANA INTEGRA NUSANTARA</li>
+        <li style="padding: 10px 25px;"><img src="{{ url('/assets/images/svg/telephone.svg') }}" width="14" height="14">+62 822-3585-0005</li>
+        <li style="padding: 10px 25px;"><img src="{{ url('/assets/images/svg/mail.svg') }}" width="14" height="14">halo@cataliz.id</li>
+        <li style="text-align:left; padding: 0px 25px;"><img src="{{ url('/assets/images/svg/place.svg') }}" width="14" height="14">Jl. Bend. Palasari No.Kav 5,<br>
+        Karangbesuki, Kec. Sukun,<br>Kota Malang, Jawa Timur 65149</li>
+      </div>
     </div>
     </footer>
   </body>
